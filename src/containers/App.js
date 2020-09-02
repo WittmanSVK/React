4845278b531b5b4ded62dabb9cfcdfb18acf51e0
Frantisek Durana <ferkys888@azet.sk>
2020-09-02 19:05:08 +0200

    first unit test

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 import Layout from '../hoc/Layout/Layout';
 import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
-import Checkout from './Checkout/Checkout';
-import Orders from './Orders/Orders';
 import Logout from '../containers/Auth/Logout/Logout';
-import Auth from '../containers/Auth/Auth';
 import {Route, withRouter, Switch, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import * as actions from '../store/actions/index';
+import asyncComponent from '../hoc/asyncComponent/asyncComponent';
+
+const asyncCheckout = asyncComponent(() => {
+    return import('./Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+    return import('./Orders/Orders');
+});
+
+const asyncAuth = asyncComponent(() => {
+    return import('../containers/Auth/Auth');
+});
 
 class App extends Component {
     componentDidMount(){
@@ -17,7 +27,7 @@ class App extends Component {
     render() {
         let routes = (
             <Switch>
-                <Route path="/auth" component={Auth}/>
+                <Route path="/auth" component={asyncAuth}/>
                 <Route path="/" exact component={BurgerBuilder}/>
                 <Redirect to="/" />
             </Switch>
@@ -26,9 +36,10 @@ class App extends Component {
         if (this.props.isAuthenticated){
             routes =
                 <Switch>
-                    <Route path="/checkout" component={Checkout}/>
-                    <Route path="/orders" component={Orders}/>
+                    <Route path="/checkout" component={asyncCheckout}/>
+                    <Route path="/orders" component={asyncOrders}/>
                     <Route path="/logout" component={Logout}/>
+                    <Route path="/auth" component={asyncAuth}/>
                     <Route path="/" exact component={BurgerBuilder}/>
                     <Redirect to="/" />
                 </Switch>
